82dbf90aa021544a9782f75a96ff2a20b188e875
aleksolh <aleksolh@stud.ntnu.no>
2023-04-10 22:40:25 +0200

    Added print and logical operations and types

--- bprog-interpreter/src/lib.rs
+++ bprog-interpreter/src/lib.rs
@@ -1,11 +1,12 @@
 pub mod interpreter {
     use std::string;
+
     use super::parser;
 
-    use super::types::{Stack, WValue as V, OpArithmetic};
+    use super::types::{Stack, WValue as V, OpBinary};
     
     /// Does the arithmetic operation sent as a parameter on the top two elements of the stack
-    pub fn op_arithmetic(stack: &mut Stack, op: OpArithmetic) -> Result<V, String> {
+    pub fn op_binary(stack: &mut Stack, op: OpBinary) -> Result<V, String> {
         if stack.len() < 2 {
             Err(format!("Not enough arguments for {:?}", op))
         } else {
@@ -14,21 +15,43 @@ pub mod interpreter {
             // Mathces the types of the top two elements on the stack with the opperation
             let result = a.and_then(|a_val| b.and_then(|b_val| {
                 match (a_val, b_val, op) {
-                    (V::VInt(a), V::VInt(b), OpArithmetic::Add) => Ok(V::VInt(a + b)),
-                    (V::VFloat(a), V::VFloat(b), OpArithmetic::Add) => Ok(V::VFloat(a + b)),
-                    (V::VInt(a), V::VInt(b), OpArithmetic::Subtract) => Ok(V::VInt(a - b)),
-                    (V::VFloat(a), V::VFloat(b), OpArithmetic::Subtract) => Ok(V::VFloat(a - b)),
-                    (V::VInt(a), V::VInt(b), OpArithmetic::Multiply) => Ok(V::VInt(a * b)),
-                    (V::VFloat(a), V::VFloat(b), OpArithmetic::Multiply) => Ok(V::VFloat(a * b)),
-                    (V::VInt(a), V::VInt(b), OpArithmetic::Divide) => Ok(V::VInt(a / b)),
-                    (V::VFloat(a), V::VFloat(b), OpArithmetic::Divide) => Ok(V::VFloat(a / b)),
+                    // Here type a matches type b
+                    (V::VInt(a), V::VInt(b), OpBinary::Add) => Ok(V::VInt(a + b)),
+                    (V::VFloat(a), V::VFloat(b), OpBinary::Add) => Ok(V::VFloat(a + b)),
+
+                    (V::VInt(a), V::VInt(b), OpBinary::Subtract) => Ok(V::VInt(a - b)),
+                    (V::VFloat(a), V::VFloat(b), OpBinary::Subtract) => Ok(V::VFloat(a - b)),
+
+                    (V::VInt(a), V::VInt(b), OpBinary::Multiply) => Ok(V::VInt(a * b)),
+                    (V::VFloat(a), V::VFloat(b), OpBinary::Multiply) => Ok(V::VFloat(a * b)),
+
+                    (V::VInt(a), V::VInt(b), OpBinary::IDivide) => Ok(V::VInt(a / b)),
+                    (V::VFloat(a), V::VFloat(b), OpBinary::FDivide) => Ok(V::VFloat(a / b)),
+
+                    (V::VInt(a), V::VInt(b), OpBinary::RGreater) => Ok(V::VBool(a < b)),
+                    (V::VFloat(a), V::VFloat(b), OpBinary::RGreater) => Ok(V::VBool(a < b)),
+
+                    (V::VInt(a), V::VInt(b), OpBinary::LGreater) => Ok(V::VBool(a > b)),
+                    (V::VFloat(a), V::VFloat(b), OpBinary::LGreater) => Ok(V::VBool(a > b)),
+
+                    (V::VInt(a), V::VInt(b), OpBinary::Equality) => Ok(V::VBool(a == b)),
+                    // Searched a bit online and found that comparing one float subtracted by another to epsilon is better
+                    // than using == which can cause problems
+                    (V::VFloat(a), V::VFloat(b), OpBinary::Equality) => Ok(V::VBool((a - b).abs() < f32::EPSILON)),
+                    (V::VBool(a), V::VBool(b), OpBinary::Equality) => Ok(V::VBool(a == b)),
+                    
+                    (V::VBool(a), V::VBool(b), OpBinary::And) => Ok(V::VBool(a && b)),
+                    (V::VBool(a), V::VBool(b), OpBinary::Or) => Ok(V::VBool(a || b)),
+
+                    // Allowed operations where types do not fully match
+                    // TODO
                     _ => Err(format!("The type is not supported for {:?} operation", op)),
                 }
             }));
             result
         }
     }   
-
+    
     /// Popps an item of the stack
     pub fn op_pop(stack: &mut Stack){
         if !stack.is_empty() {
@@ -52,8 +75,9 @@ pub mod interpreter {
             }
         }
     }
+
     /// Duplicates the top element of the stack
-    pub fn op_dup(stack: &mut Stack, tokens: &[&str], ignore: bool){
+    pub fn op_dup(stack: &mut Stack, ignore: bool, tokens: &[&str]){
         if stack.len() >= 1 {
             let top_element = stack[0].clone();
             stack.push(top_element);
@@ -64,8 +88,9 @@ pub mod interpreter {
 
         parser::process_tokens(&tokens[1..], ignore, stack);
     }
+
     /// Swaps the order of the top two elements on the stack
-    pub fn op_swap(stack: &mut Stack, tokens: &[&str], ignore: bool) {
+    pub fn op_swap(stack: &mut Stack, ignore: bool, tokens: &[&str]) {
         if stack.len() >= 2 {
             stack.swap(0, 1);
         }
@@ -76,11 +101,13 @@ pub mod interpreter {
     }
 
     /// Prints the top element on the stack, works only for String types
-    pub fn op_print(stack: &mut Stack, tokens: &[&str], ignore: bool){
+    pub fn op_print(stack: &mut Stack, ignore: bool, tokens: &[&str]){
         if let Some(top_element) = stack.get(0) {       // Get top element if there is one 
             match top_element {
                 Ok(V::VString(s)) => {                  // Top element is of type String
                     println!("{}", s);
+                    // We check that there is an element above so do not need to use the Result return type
+                    stack.remove(0);                    // Remove top element after printing it
                 }
                 Ok(_) => {
                     println!("Error: print not supported for other types than VString!");
@@ -91,6 +118,29 @@ pub mod interpreter {
             }
         } else {
             println!("Error: stack is empty, no top element to print!");
+        }
+        parser::process_tokens(&tokens[1..], ignore, stack);
+    }
+
+    /// Changes the top element to the not of it
+    pub fn op_not(stack: &mut Stack, ignore: bool, tokens: &[&str]){
+        if let Some(top_element) = stack.get_mut(0) {
+            match top_element {
+                Ok(V::VBool(b)) => {       
+                    *b = !*b;                 
+                }
+                Ok(V::VInt(i)) => {       
+                    *i = -*i;                 
+                }
+                Ok(_) => {
+                    println!("Error: print not supported for other types than VString!");
+                }
+                Err(_) => {
+                    println!("Error: there is an error value at the top of the stack!");
+                }
+            }
+        } else {
+            println!("Error: stack is empty, no top element to perform logical not on!")
         }
         parser::process_tokens(&tokens[1..], ignore, stack);
     }
@@ -128,8 +178,9 @@ pub mod interpreter {
 
  
 pub mod parser {
+
     use super::interpreter;
-    use super::types::{Stack, WValue, OpArithmetic};
+    use super::types::{Stack, OpBinary};
     
     /// Parses the string input into "tokens" for example *, +, then calls the process_tokens function to 
     /// execute the corresponding code depending on the tokens. 
@@ -144,14 +195,21 @@ pub mod parser {
     pub fn process_tokens(tokens: &[&str], ignore: bool, stack: &mut Stack) {
         if !tokens.is_empty() {
             match tokens[0] {
-                "*" if !ignore => exectue_arithmetic_op(stack, OpArithmetic::Multiply, ignore, &tokens), 
-                "+" if !ignore => exectue_arithmetic_op(stack, OpArithmetic::Add, ignore, &tokens), 
-                "-" if !ignore => exectue_arithmetic_op(stack, OpArithmetic::Subtract, ignore, &tokens),
-                "/" if !ignore => exectue_arithmetic_op(stack, OpArithmetic::Divide, ignore, &tokens),
+                "*" if !ignore => exectue_binary_op(stack, OpBinary::Multiply, ignore, &tokens), 
+                "+" if !ignore => exectue_binary_op(stack, OpBinary::Add, ignore, &tokens), 
+                "-" if !ignore => exectue_binary_op(stack, OpBinary::Subtract, ignore, &tokens),
+                "/" if !ignore => exectue_binary_op(stack, OpBinary::FDivide, ignore, &tokens),
+                "div" if !ignore => exectue_binary_op(stack, OpBinary::IDivide, ignore, &tokens),
+                "<" if !ignore => exectue_binary_op(stack, OpBinary::RGreater, ignore, &tokens),
+                ">" if !ignore => exectue_binary_op(stack, OpBinary::LGreater, ignore, &tokens),
+                "==" if !ignore => exectue_binary_op(stack, OpBinary::Equality, ignore, &tokens),
+                "&&" if !ignore => exectue_binary_op(stack, OpBinary::And, ignore, &tokens),
+                "||" if !ignore => exectue_binary_op(stack, OpBinary::Or, ignore, &tokens),
+                "not" if !ignore => interpreter::op_not(stack, ignore, &tokens),
                 "\"" if !ignore => interpreter::op_quotes(stack, ignore, &tokens), 
-                "dup" if !ignore => interpreter::op_dup(stack),
-                "swap" if !ignore => interpreter::op_swap(stack),
-                "print" if !ignore => interpreter::op_print(stack),
+                "dup" if !ignore => interpreter::op_dup(stack, ignore, &tokens),
+                "swap" if !ignore => interpreter::op_swap(stack, ignore, &tokens),
+                "print" if !ignore => interpreter::op_print(stack, ignore, &tokens),
                 "pop" if !ignore => {
                     interpreter::op_pop(stack);
                     process_tokens(&tokens[1..], ignore, stack);
@@ -165,9 +223,9 @@ pub mod parser {
         }
     }
 
-    fn exectue_arithmetic_op(stack: &mut Stack, op: OpArithmetic, ignore: bool, tokens: &[&str]) {
+    fn exectue_binary_op(stack: &mut Stack, op: OpBinary, ignore: bool, tokens: &[&str]) {
         if !ignore {
-            match interpreter::op_arithmetic(stack, op) {
+            match interpreter::op_binary(stack, op) {
                 Ok(val) => {                // The arithemetic operation was possible and allowed for types
                     stack.remove(0);        // remove top two elements that computed the value
                     stack.remove(0);
@@ -188,12 +246,18 @@ pub mod types {
     // Represents the program stack
     pub type Stack = Vec<Result<WValue, String>>;
 
-    #[derive(Debug, Copy, Clone)]            // For printing out the OpArithmetic, for example Add.
-    pub enum OpArithmetic {
+    #[derive(Debug, Copy, Clone)]            // For printing out the OpBinary, for example Add.
+    pub enum OpBinary {
         Add,
         Subtract,
         Multiply,
-        Divide,
+        FDivide,
+        IDivide,
+        RGreater,
+        LGreater,
+        Equality,
+        And,
+        Or 
     }
     #[derive(Clone)]
     pub enum WValue {
